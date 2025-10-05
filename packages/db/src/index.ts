adcb0e732f12992e46af2a154eabc8de9824c32d
Morgan Stovold <morgannstovold.dev@gmail.com>
2025-10-05 13:17:19 -0700

    chore: remove prisma adapter and relationMode config, upgrade turbo to 2.5.8

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,15 +1,11 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 import { env } from "./env";
-
-console.log(env.DATABASE_URL);
 
 const createPrismaClient = () =>
 	new PrismaClient({
 		log:
 			env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-		adapter: new PrismaPg({ connectionString: env.DATABASE_URL }),
 	});
 
 const globalForPrisma = globalThis as unknown as {
