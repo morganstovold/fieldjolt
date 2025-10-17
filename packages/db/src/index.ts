58e57cb1579bca24102b4847bc4364a5fe83eaa8
Morgan Stovold <morgannstovold.dev@gmail.com>
2025-10-16 23:00:34 -0700

    feat: configure Prisma with PostgreSQL adapter and update client dependencies

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,13 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { env } from "./env";
 import { PrismaClient } from "./generated/client";
 
-const createPrismaClient = () => new PrismaClient();
+const createPrismaClient = () =>
+	new PrismaClient({
+		adapter: new PrismaPg({
+			connectionString: env.DATABASE_URL,
+		}),
+	});
 
 const globalForPrisma = globalThis as unknown as {
 	prisma: ReturnType<typeof createPrismaClient> | undefined;
